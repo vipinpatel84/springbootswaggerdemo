be2889bbae27fd8a510f819c4f88260b63ee9648
vipin patel <vipin.vipinpatel84gmail.com>
2022-04-21 00:12:42 +0200

    initial project commit

---
+++ src/main/java/com/springboot/swagger/SwaggerApplication.java
@@ -0,0 +1,21 @@
+package com.springboot.swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SwaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerApplication.class, args);
+	}
+
+
+	@GetMapping("/")
+	public String getHelloSwagger(){
+		return "Hello Swaggger";
+	}
+}
